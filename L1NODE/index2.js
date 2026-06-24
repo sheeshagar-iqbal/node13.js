d4c3e9ff625d5c25afaa1e6fcc6927593cc0b297
sheeshagar-iqbal <iqbalkhan234786@gmail.com>
2026-06-24 12:41:24 +0530

    ejs template and use form submition

--- L1NODE/index2.js
+++ L1NODE/index2.js
@@ -13,14 +13,44 @@ app.use(express.json())
 // accept form data   post method
 app.use(express.urlencoded({extended:false}))
 
+//  for static file
+app.use (express.static('public'))
+
 app.get('/',(req,res)=>{
     res.send('hi')
 })
 
 app.get('/user',(req,res)=>{
-    res.render('user', {title:'hello body', message:'what are you doing'})
+    const user= [
+    {
+        "name":"iqbal",
+        "age":23
+    },{
+        "name":"khan",
+        "age":23
+    },{
+        "name":"sheeshgar",
+        "age":23
+    }
+
+]
+const items =['apple','banana','orange','mango']
+    res.render('user', {title:'hello body', message:'what are you doing',items,user})
 })
 
+
+
+app.get('/form',(req,res)=>{
+    res.render('form',{messsage:null})
+})
+
+app.post('/submit',(req,res)=>{
+    // res.render('form')
+    const data= req.body.myname
+    const messsage= `${data} your form submitted`
+    // res.send(data)
+    res.render('form',{messsage:messsage})
+})
 
 
 app.listen(4000,()=> console.log('server run sucessfully')
