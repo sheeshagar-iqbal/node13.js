e7cd41a9ee47940d043634b4de604249c596924e
sheeshagar-iqbal <iqbalkhan234786@gmail.com>
2026-06-28 09:07:29 +0530

    use try catch method for error handling

--- L1NODE/controller/contacts.controller.js
+++ L1NODE/controller/contacts.controller.js
@@ -2,21 +2,35 @@ import mongoose from 'mongoose'
 import Contact from '../models/contacts.model.js'
 
 export const getContacts=async (req,res)=>{
-        const contact=   await Contact.find()
+    try {
+         const contact=   await Contact.find()
             // res.json(contact)
         res.render('home',{contact})
+    } catch (error) {
+        res.render('500',{message: error})
+    }
+       
 }
 
 export const showContact =async(req,res)=>{
         var paramId = mongoose.Types.ObjectId.isValid(req.params.id)
     if(!paramId){
          return res.render('404',{message :'invalid id'})
-    }    
-
-    // const contact = await Contact.findOne({_id:req.params.id})
+    } 
+    
+    try {
+          // const contact = await Contact.findOne({_id:req.params.id})
     const contact = await Contact.findById(req.params.id)
+    if(!contact) return   res.render('404',{message :'contact not found'})
     // res.json(contact)
     res.render('show-contact', {contact})
+        
+    } catch (error) {
+        res.render('500',{message: error})
+        
+    }
+
+  
 
 }
 
@@ -27,7 +41,9 @@ export const addContactPage=(req,res)=>{
 
 
 export const addContact=async(req,res)=>{
-    // res.send(req.body)
+   
+            try { 
+                // res.send(req.body)
         await Contact.insertOne({
             first_name:req.body.first_name,
             last_name:req.body.last_name,
@@ -36,6 +52,10 @@ export const addContact=async(req,res)=>{
             address:req.body.address
         })
         res.redirect('/')
+    
+    } catch (error) {
+        res.render('500',{message: error})
+    }
 
 }
 
@@ -44,11 +64,17 @@ export const updatecontactpage=async (req,res)=>{
     if(!paramId){
          return res.render('404',{message :'invalid id'})
     } 
-
-    // console.log("Original URL:", req.originalUrl);
+    try {
+            // console.log("Original URL:", req.originalUrl);
     // console.log("ID:", req.params.id);
      const contact = await Contact.findById(req.params.id)
+    if(!contact) return   res.render('404',{message :'contact not found'})
     res.render('update-contact',{contact})
+    } catch (error) {
+        res.render('500',{message: error})
+    }
+
+
 
 
 }
@@ -58,8 +84,16 @@ export const updateContact=async(req,res)=>{
     if(!paramId){
          return res.render('404',{message :'invalid id'})
     } 
-     await Contact.findByIdAndUpdate(req.params.id , req.body)
-     res.redirect('/')
+
+      try {
+            // console.log("Original URL:", req.originalUrl);
+    // console.log("ID:", req.params.id);
+     const contact =await Contact.findByIdAndUpdate(req.params.id , req.body)
+    if(!contact) return   res.render('404',{message :'contact not found'})
+         res.redirect('/')
+    } catch (error) {
+        res.render('500',{message: error})
+    }
 
 }
 
@@ -68,8 +102,16 @@ export const deleteContact= async(req,res)=>{
     if(!paramId){
          return res.render('404',{message :'invalid id'})
     } 
-     await Contact.findByIdAndDelete(req.params.id)
-     res.redirect('/')
+
+     try {
+     const contact =await Contact.findByIdAndDelete(req.params.id)
+    if(!contact) return   res.render('404',{message :'contact not found'})
+         res.redirect('/')
+    } catch (error) {
+        res.render('500',{message: error})
+    }
+     
+     
 
 
 }
