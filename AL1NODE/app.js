6aa8f9012391b820ab0901d978df72460b19bc2f
sheeshagar-iqbal <iqbalkhan234786@gmail.com>
2026-07-06 15:45:14 +0530

    fetch

--- AL1NODE/app.js
+++ AL1NODE/app.js
@@ -4,10 +4,19 @@ import studentrouter from './routes/students.route.js'
 import mongodb from './config/mongodbconnecton.js'
 import { error } from 'console'
 import { MulterError } from 'multer'
-
-
+import cors from 'cors'
+import { fileURLToPath } from "url";
+import path from 'path'
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+
+app.use(cors())
+// app.use ("/uploads",express.static(path.join(__filename,"upload")))
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/uploads", express.static(path.join(__dirname, "upload")));
 
 app.use('/api/student',studentrouter)
 
